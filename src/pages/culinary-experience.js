36aa904db2e888b2609601721e45164843e46e0b
Gilbert <gilbertlucas46@gmail.com>
2019-05-26 18:25:25 +0800

    added images

--- src/pages/culinary-experience.js
+++ src/pages/culinary-experience.js
@@ -2,16 +2,33 @@ import React from "react"
 
 import Layout from "../components/layout"
 import styled from 'styled-components'
-
+import 'rc-tabs/assets/index.css';
+import Tabs, { TabPane } from 'rc-tabs';
+import TabContent from 'rc-tabs/lib/TabContent';
+import ScrollableInkTabBar from 'rc-tabs/lib/ScrollableInkTabBar';
 
 const CulinaryContainer = styled.div`
 
 `;
 
+const callback = function(key){
+ 
+}
+
 const CulinaryExperience = ({location}) => (
   <Layout location={location}>
     <article>
       <CulinaryContainer className="container">
+        <Tabs
+        defaultActiveKey="2"
+        onChange={callback}
+        renderTabBar={()=><ScrollableInkTabBar />}
+        renderTabContent={()=><TabContent />}
+        >
+          <TabPane tab='tab 1' key="1">first</TabPane>
+          <TabPane tab='tab 2' key="2">second</TabPane>
+          <TabPane tab='tab 3' key="3">third</TabPane>
+        </Tabs>
       </CulinaryContainer>
     </article>
   </Layout>
